88e87d26216a38f653b852e6e0586ee83b999216
rayct <rayct@icloud.com>
2024-04-17 17:22:31 -0400

    first commit

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const CHANNEL_NAME = '#raycturner';
+export const OAUTH_TOKEN = 'oauth:0jtj9lhmk8fb5s0q2lb6rgez5xoajf';
+export const BOT_USERNAME = 'RayBoto';
+export const BLOCKED_WORDS = [
+  'cats',
+  'dogs',
+]
